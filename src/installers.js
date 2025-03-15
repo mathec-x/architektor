8501c55d14cbacbeb0fbcc85763e96c9e0eec7fd
matheus correa <mathec@live.com>
2025-03-15 13:10:22 -0300

    fix: add default jest setup

--- src/installers.js
+++ src/installers.js
@@ -62,6 +62,7 @@ export class Installers {
   async defaultConfig() {
     this.fileManager.makeJsonFileIfNotExists(".prettierrc", settings.prettier);
     this.fileManager.makeJsonFileIfNotExists(".gitignore", settings.gitignore);
+    this.fileManager.cpFromPackageToRepo("/defaults/jest.config.js", "jest.config.js");
     this.fileManager.cpFromPackageToRepo("/defaults/.dockerignore", ".dockerignore");
     this.fileManager.cpFromPackageToRepo("/defaults/Dockerfile", "Dockerfile");
     this.fileManager.cpFromPackageToRepo("/defaults/docker-compose.yml", "docker-compose.yml");
